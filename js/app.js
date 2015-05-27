642ebf194ae0ee3323dfcc78d87eb9ffa6d7f0ca
Josiah Parent <vasti009@gmail.com>
2015-05-27 09:56:10 +0100

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var TrueSight = (function(){
+
+    var spotlight = videojs('spotlightVideo'),
+        defaultVol;
+
+    if(localStorage.getItem("volume") == undefined ){
+        localStorage.setItem("volume", 0.5);
+    }
+
+    defaultVol = localStorage.getItem("volume");
+
+    spotlight.volume(defaultVol);
+
+    this.rememberVolume = function(){
+        if(spotlight.volume() != defaultVol){
+            localStorage.setItem("volume", spotlight.volume());
+        }
+    }
+
+    // if browser support local storage, begin to remember the volume.
+    setInterval(rememberVolume, 5000);
+
+})()
