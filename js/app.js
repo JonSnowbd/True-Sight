530786ce2a958c9b9c8c3742c5ce2b16714a1cb2
Josiah Parent <vasti009@gmail.com>
2015-05-27 11:47:59 +0100

    Added Volume Sync to all vids

--- js/app.js
+++ js/app.js
@@ -1,25 +1,62 @@
 var TrueSight = (function(){
 
-    var spotlight = videojs('spotlightVideo'),
-        defaultVol;
+    this.videos = [
+        {
+            vidname:"spotlightVideo",
+            hero:"Kunkka",
+            poster:"static/image/poster/"+this.hero+".jpg"
+        },
+        {
+            vidname:"8CullingBlade",
+            hero:"Axe",
+            poster:"static/image/poster/"+this.hero+".jpg"
+        }
+    ];
 
-    if(localStorage.getItem("volume") == undefined ){
-        localStorage.setItem("volume", 0.5);
+    this.volume = 0.5;
+
+    var spotlight = videojs('spotlightVideo');
+    var parent = this;
+
+    this.init = function(){
+
+        // If volume isnt defined in storage, set it to half.
+        if(localStorage.getItem("volume") == undefined ){
+            localStorage.setItem("volume", 0.5);
+        }
+        parent.volume = localStorage.getItem("volume");
+
+        // Go through each video and set the volume to the stored volume.
+        parent.videos.forEach(function(vid){
+
+            videojs(vid.vidname).ready(function(){
+                this.volume(parent.volume);
+            });
+
+        });
     }
 
-    defaultVol = localStorage.getItem("volume");
-
-    spotlight.ready(function(){
-        this.volume(defaultVol);
-    });
-
     this.rememberVolume = function(){
-        if(spotlight.volume() != defaultVol){
-            localStorage.setItem("volume", spotlight.volume());
-        }
+
+        // Check if any video has changed its volume
+        parent.videos.forEach(function(vid){
+
+            if(videojs(vid.vidname).volume() != parent.volume){
+
+                localStorage.setItem("volume", videojs(vid.vidname).volume());
+                parent.volume = videojs(vid.vidname).volume();
+
+                // Another for each due to it not updating any video before detection.
+                parent.videos.forEach(function(vid){
+                    videojs(vid.vidname).volume(parent.volume);
+                });
+            }
+
+        });
     }
 
     // if browser support local storage, begin to remember the volume.
-    setInterval(rememberVolume, 5000);
+    setInterval(rememberVolume, 2000);
+    this.init();
 
 })()
